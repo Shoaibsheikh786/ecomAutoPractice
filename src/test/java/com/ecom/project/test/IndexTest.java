e6a578c53b8c050b13b82ecb2fe87e10499359e2
Muneer Ahmad <shoaib786asme@gmail.com>
2024-03-13 11:30:37 +0530

    Folder structre created

---
+++ src/test/java/com/ecom/project/test/IndexTest.java
@@ -0,0 +1,24 @@
+package com.ecom.project.test;
+
+import org.testng.annotations.Test;
+
+import com.ecom.project.base.SetUp;
+
+public class IndexTest extends SetUp {
+	
+	
+	IndexTest() {
+		
+	}
+  //IndexTest obj=new IndexTest();
+	
+	@Test
+	public void test1()                // 1. constructor --> readConfigFile 
+	{  
+      intilization();
+      
+      
+      ///out test will on that browser 
+	}
+
+}
